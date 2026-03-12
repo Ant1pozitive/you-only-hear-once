6f95a00d582ca3d675eb2bf28e0a9f93556cef15
Antipositive <148214981+Ant1pozitive@users.noreply.github.com>
2026-03-12 17:12:21 +0300

    Fix freq [0,1] -> mel bins

--- utils/visualize.py
+++ utils/visualize.py
@@ -2,12 +2,18 @@ import matplotlib.pyplot as plt
 import numpy as np
 import librosa.display
 
-def plot_spectrogram_with_preds(spec: np.ndarray, preds: dict, save_path: str = None):
-    fig, ax = plt.subplots()
-    librosa.display.specshow(spec, ax=ax, x_axis='time', y_axis='mel')
-    # Draw boxes/masks
+def plot_spectrogram_with_preds(spec: np.ndarray, preds: dict, n_mels: int = 128, save_path: str = None):
+    """freq [0,1] -> mel bins"""
+    fig, ax = plt.subplots(figsize=(10, 6))
+    librosa.display.specshow(spec, ax=ax, x_axis='time', y_axis='mel', sr=44100, hop_length=512)
     for box in preds.get('boxes', []):
-        ax.add_patch(plt.Rectangle((box[0], box[1]), box[2]-box[0], box[3]-box[1], fill=False, color='r'))
+        t0, f0, t1, f1 = box.tolist()
+        f0_mel = f0 * n_mels
+        f1_mel = f1 * n_mels
+        ax.add_patch(plt.Rectangle((t0, f0_mel), t1 - t0, f1_mel - f0_mel,
+                                   fill=False, color='red', linewidth=2))
+    ax.set_title("YOHO Predictions")
     if save_path:
-        plt.savefig(save_path)
+        plt.savefig(save_path, dpi=300, bbox_inches='tight')
+    plt.show()
     plt.close()
